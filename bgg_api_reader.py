c4ff883016ef4cf3c7970c1a07938a8b72f2d4b4
szaroket <30178095+szaroket@users.noreply.github.com>
2022-08-14 15:16:06 +0200

    Add simple API requests to get list of game ids

---
+++ bgg_api_reader.py
@@ -0,0 +1,20 @@
+import requests
+import xmltodict
+
+
+def get_all_gameid(response: dict) -> dict:
+    list_of_gameid = []
+    for game in response["boardgames"]["boardgame"]:
+        list_of_gameid.append(game["@objectid"])
+    return list_of_gameid
+
+
+def get_gameid_from_api(game_name: str) -> None:
+    response = requests.get("https://api.geekdo.com/xmlapi/search?search=" + game_name)
+    dict_searched_gamesid = xmltodict.parse(response.content)
+    print(response.content)
+    print(dict_searched_gamesid)
+    print(get_all_gameid(dict_searched_gamesid))
+
+
+get_gameid_from_api("Everdell")
