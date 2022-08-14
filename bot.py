058b4bd3d81ebce15b16f8b05ab8b2db2fbad60a
szaroket <30178095+szaroket@users.noreply.github.com>
2022-08-14 14:28:28 +0200

    Format file with black

--- bot.py
+++ bot.py
@@ -4,22 +4,24 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text='Hello! I\'m BGG Telegram Bot. What game are you looking for?')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Hello! I'm BGG Telegram Bot. What game are you looking for?",
+    )
 
 
 async def search_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # todo: add logic for searching board games using BGG API
     print(update.message.text)
-    await update.message.reply_text(text=f'Here are results for {update.message.text}')
+    await update.message.reply_text(text=f"Here are results for {update.message.text}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(config.bot_token).build()
 
     #  Handling bot commands
-    start_handler = CommandHandler(['hello', 'start'], start)
-    search_handler = CommandHandler('search', search_game)
+    start_handler = CommandHandler(["hello", "start"], start)
+    search_handler = CommandHandler("search", search_game)
 
     application.add_handler(start_handler)
     application.add_handler(search_handler)
